f0b4f63a58b40fb9fe1a5a035a6fe688e4e3bdc4
wenjye <wenjye00@gmail.com>
2021-01-12 17:10:17 +0800

    Initial commit

---
+++ segp/project_1/myapp/avgReaderCountScript.py
@@ -0,0 +1,59 @@
+from mendeley import Mendeley
+from datetime import datetime #current date and time
+
+def all_subset(query):
+    from itertools import combinations
+    subset = []
+    if len(query) > 2:
+        for i in set(combinations(query, r=2)):
+            subset.append(i)
+        for i in set(combinations(query, r=1)):
+            subset.append(i)
+    else:
+        for i in set(combinations(query, r=1)):
+            subset.append(i)
+    return subset
+
+def avgRCSList(queryList):
+    client_id = "8910"
+    client_secret = "OxP464bB5roU81GH"
+    avgRCS =0
+    curryear = datetime.now().year #get the current year
+    mendeley = Mendeley(client_id=client_id, client_secret=client_secret)
+    resultList = []
+
+    auth = mendeley.start_client_credentials_flow()
+    session = auth.authenticate()
+    queryList_subset = all_subset(queryList)
+    for query in queryList_subset:
+        x = 0
+        iterCount = 0
+        paperCount = 0
+        avgRCS = 0
+        avgyear = 0
+        pages = session.catalog.advanced_search(source=query, view="stats")
+
+        for page in pages.iter(page_size=100):
+            if iterCount == 1000:
+                break
+            if curryear - page.year <= 0:
+                x += 0
+            else:
+                x += page.reader_count / (curryear - page.year)
+                paperCount += 1
+            iterCount += 1
+            avgyear += page.year #calculate average year of publications
+
+        avgyear = avgyear / iterCount
+        avgRCS = round((x / paperCount),2)
+        resultList.append(avgRCS)
+        print(str(query) + ": " + str(avgRCS) + " | published: " + str(paperCount) + " | avgyear: " + str(round(avgyear)))
+
+        results = {
+
+            'subset':queryList_subset,
+            'marks':resultList,
+            'zipped':zip(queryList_subset,resultList),
+        }
+
+    return results
---
+++ segp/project_1/myapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from myapp import avgReaderCountScript
+
+#from django.shortcuts import render,redirect
+#from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'WebsiteSEGP.html')
+    
+def about(request):
+    return render(request, 'About.html')
+    
+def topic(request):
+    return render(request, 'Topics.html')
+    
+def case1(request):
+    return render(request, 'Case1.html')
+
+def case2(request):
+    return render(request, 'Case2.html')
+
+def results1(request):
+    submitted_query = []
+    if request.method == 'POST':
+        query = str(request.POST['input_submitted'])
+        a = query.splitlines()
+        for i in a:
+             i=i.replace('× ', '')
+             i=i.replace('×', '')
+             if i !="":
+                submitted_query.append((str(i)))
+    context = avgReaderCountScript.avgRCSList(submitted_query)
+    return render(request, 'Results1.html',context)
+
+def results2(request):
+    return render(request, 'Results2.html')
