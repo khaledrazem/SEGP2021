b3ee25ebba1603502bbb37ae136d92f1998d280a
wenjye <wenjye00@gmail.com>
2021-03-11 22:15:40 +0800

    'nlp_test_modified'

--- segp/project_1/myapp/elsevier_test.py
+++ segp/project_1/myapp/elsevier_test.py
@@ -1,31 +1,33 @@
-"""An example program that uses the elsapy module"""
-
-from elsapy.elsclient import ElsClient
-from elsapy.elsprofile import ElsAuthor, ElsAffil
-from elsapy.elsdoc import FullDoc, AbsDoc
-from elsapy.elssearch import ElsSearch
-from collections import Counter
-import json
-
-
-def elsevier_auth():
-    client = ElsClient("1ebaeb2ea719e96071ce074a5c341963")
-    client.inst_token = "6383ea4db27ea6b7353107935f098932"
-    return client
-
-client = elsevier_auth()
-myDocSrch = ElsSearch('Transportation + Architecture','sciencedirect')
-myDocSrch.execute(client,get_all = False)
-text=''
-for i in myDocSrch.results:
-    print(i['dc:title'])
-    pii_doc = FullDoc(sd_pii=i['pii'])
-    if pii_doc.read(client):
-        print(pii_doc.data['coredata']['dc:description'])
-        # text += pii_doc.data['coredata']['dc:description']
-# split_it = text.split()
-# Counter = Counter(split_it)
-# most_occur = Counter.most_common(40)
-# print(most_occur)
-# pii_doc.data['coredata']['dc:description']
-        # prism: coverDisplayDate
+# """An example program that uses the elsapy module"""
+# from elsapy.elsclient import ElsClient
+# from elsapy.elsprofile import ElsAuthor, ElsAffil
+# from elsapy.elsdoc import FullDoc, AbsDoc
+# from elsapy.elssearch import ElsSearch
+# from collections import Counter
+# import json
+#
+#
+# def elsevier_auth():
+#     client = ElsClient("1ebaeb2ea719e96071ce074a5c341963")
+#     client.inst_token = "6383ea4db27ea6b7353107935f098932"
+#     return client
+#
+# client = elsevier_auth()
+# myDocSrch = ElsSearch('Transportation + Architecture','sciencedirect')
+# myDocSrch.execute(client,get_all = False)
+# document_doi =[]
+# for i in myDocSrch.results:
+#     print(i['dc:title'])
+#     document_doi.append(i['pii'])
+# text=''
+# for j in document_doi:
+#     pii_doc = FullDoc(sd_pii=j)
+#     if pii_doc.read(client):
+#         text += pii_doc.data['coredata']['dc:description']
+# print(text)
+# # split_it = text.split()
+# # Counter = Counter(split_it)
+# # most_occur = Counter.most_common(40)
+# # print(most_occur)
+# # pii_doc.data['coredata']['dc:description']
+#         # prism: coverDisplayDate
